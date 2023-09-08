4b35e500500f1a3dcbb5ef32c530640acf8857bb
Vaijinath789 <vaijinathmalge929@gmail.com>
2023-09-08 16:49:09 +0530

    vgrgr

--- Vaiji/src/com/Test1/Qustion4.java
+++ Vaiji/src/com/Test1/Qustion4.java
@@ -1,19 +1,23 @@
 package com.Test1;
 
 public class Qustion4 {
-	public static void show(int[]a) {
-		int result=0;
-		for(int i=0;i<a.length-1;i++) {
-			 result= result+a[i];
-			if(result==11) {
-				System.out.println("still index= "+i);
+	public static void show(int[]a,int num) {
+		for(int i=0;i<a.length;i++) {
+			int result=a[i];			
+			for(int j=i+1;j<a.length;j++) {
+				result=result+a[j];	
+				if(result==num) {
+					System.out.println(""+i+","+j+"");
+				}
 			}
-			
 		}
+			
 	}
+	
 	public static void main(String[] args) {
 		int []arr= {2,3,6,4,9,0,11};
-		show(arr);
+		int num=11;
+		show(arr,num);
 	}
 
 }
